994c086bb19de892705ddf33bc0b9e8c51558c2f
w.tknv <rreedd555@gmail.com>
2025-06-02 20:57:18 +0800

    remove ACCESS_NETWORK_STATE premission and update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "lab.rreedd.oyatsu"
         minSdk = 21
         targetSdk = 35
-        versionCode = 7
-        versionName = "3.1"
+        versionCode = 8
+        versionName = "3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
